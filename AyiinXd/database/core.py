a1e9819a4edcfb176abbf6b554eb2a07ee52b11e
yusniiii <83091198+yusniii@users.noreply.github.com>
2023-11-26 16:38:16 +0800

    Update core.py

--- AyiinXd/database/core.py
+++ AyiinXd/database/core.py
@@ -1,12 +1,12 @@
 # Ayiin - Userbot
 # Copyright (C) 2022-2023 @AyiinXd
 #
-# This file is a part of < https://github.com/AyiinXd/Ayiin-Userbot >
+# This file is a part of < https://github.com/yusniii/Yxzn-Userbot >
 # PLease read the GNU Affero General Public License in
-# <https://www.github.com/AyiinXd/Ayiin-Userbot/blob/main/LICENSE/>.
+# <https://www.github.com/yusniii/Yxzn-Userbot/blob/main/LICENSE/>.
 #
-# FROM Ayiin-Userbot <https://github.com/AyiinXd/Ayiin-Userbot>
-# t.me/AyiinChats & t.me/AyiinChannel
+# FROM Ayiin-Userbot <https://github.com/yusniii/Yxzn-Userbot>
+# t.me/mutualanstaryy & t.me/storeyxzn
 
 
 # ========================×========================
