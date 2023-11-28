1932950e7081c27b06ef7e4c21d10627b01aea01
yusniiii <83091198+yusniii@users.noreply.github.com>
2023-11-28 20:16:05 +0800

    Update quotly.py

--- AyiinXd/modules/quotly.py
+++ AyiinXd/modules/quotly.py
@@ -229,7 +229,7 @@ async def quotly(event):
         file = await create_quotly(reply_, bg=match, reply=replied_to, sender=user)
     except Exception as er:
         return await msg.edit("**KESALAHAN : **`{}`".format(er))
-    message = await reply.reply("Quotly by Ayiin-Userbot", file=file)
+    message = await reply.reply("Quotly by Yxzn-Userbot", file=file)
     remove(file)
     await msg.delete()
     return message
